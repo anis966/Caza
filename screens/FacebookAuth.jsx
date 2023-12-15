071c237b080f81ca5d81818cf66f3146c5e8b22d
Anis slimeni <anis.ben.mohamed@tekab.dev>
2023-12-15 10:53:36 +0100

    change fb login function

--- screens/FacebookAuth.jsx
+++ screens/FacebookAuth.jsx
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Button } from 'react-native';
-import { loginInWithPermissions } from '@fadidev/react-native-fblogin';
-import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
+import * as Facebook from 'expo-auth-session/providers/facebook';
+import * as AuthSession from 'expo-auth-session';
+import { ResponseType } from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
 
-class FacebookAuth extends Component {
-  loginIn = () => {
-    return loginInWithPermissions({
-      runNow: true,
-      redirectUrl: 'https://facebook.com/connect/login_success.html',
-      getMyInformationsFields: ['id,first_name,last_name,name,email,picture'],
-      clientId: '1103569104349638',
-      secretKey: 'ba96e245e20b8a1b5b64a9d03efb08a7',
-      onLoginSuccess: data => console.log(data),
-      onLoginFailure: error => console.log(error),
-    });
-  };
+WebBrowser.maybeCompleteAuthSession();
 
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'space-evenly', width: '50%', alignSelf: 'center', marginVertical: '10%' }}>
-        <Button onPress={this.loginIn} title="Login" />
-      </View>
-    );
-  }
+
+export default function FacebookAuth() {
+  const [request, response, prompt] = Facebook.useAuthRequest(
+    {
+      clientId: "1103569104349638",
+      responseType: ResponseType.Code,
+    }
+
+  );
+
+  React.useEffect(() => {
+    console.log("response: => ", response);
+    if (response?.type === 'success') {
+      const { code } = response.params;
+
+       const fetchData = async () => {
+        
+        const requestOptions = {
+          method: 'GET',
+          headers: {
+            "Content-Type": "application/json"
+          }
+        };
+
+        const link = "https://graph.facebook.com/v7.0/oauth/access_token" +
+        "?client_id=*******************" +
+        "&redirect_uri=https://auth.expo.io/@********/**********" +
+        "&client_secret=*******************" +
+        "&code=" + code;
+        
+        const response = await fetch(link, requestOptions);
+        const body = await response.json();
+        console.log("fetchData response: => ", body);
+      }
+
+      fetchData().catch(console.error);
+    }
+  }, [response]);
+  
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'space-evenly', width: '50%', alignSelf: 'center', marginVertical: '10%' }}>
+      <Button onPress={() => prompt()} title="Login with FB" />
+    </View>
+  );
 }
-
-export default FacebookAuth;
